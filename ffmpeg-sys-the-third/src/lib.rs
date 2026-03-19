9f013dabb738e23546060d4644e5aa18520841f7
Alex Parrill <alex.parrill@col32.net>
2026-03-19 16:11:19 -0400

    Silence rustdoc warnings in the sys crate
    The docs are lifted from the C source code and don't correspond to what
    rustdoc is expecting.

--- ffmpeg-sys-the-third/src/lib.rs
+++ ffmpeg-sys-the-third/src/lib.rs
@@ -7,6 +7,9 @@
 #![allow(clippy::redundant_static_lifetimes)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::type_complexity)]
+#![allow(rustdoc::bare_urls)]
+#![allow(rustdoc::broken_intra_doc_links)]
+#![allow(rustdoc::invalid_html_tags)]
 
 // see rust-lang/rust-bindgen#3241
 #![allow(unnecessary_transmutes)]
